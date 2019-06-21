d94f2f8051a47f3c7815b7b80b3e1b9eb0f971ea
Inyourdreams <romanchenkopv@gmail.com>
2019-06-21 17:55:51 +0300

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import ListingPage from './components/ListingPage/ListingPage'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ListingPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ListingPage/ListingPage.js
@@ -0,0 +1,104 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import './ListingPage.css'
+import Loader from 'react-loader-spinner'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import { connect } from 'react-redux'
+import { setListingData, setIndex } from '../../modules/listingReducer'
+
+const ListingPage = ({
+  setListingData,
+  listingData,
+  idx,
+  setIndex,
+  search,
+}) => {
+  const fetchNextData = index => {
+    return axios(`/CONTENTLISTINGPAGE-PAGE${index}.json`).then(
+      res => res.data.page['content-items'].content,
+    )
+  }
+
+  const setListing = () => {
+    fetchNextData(idx).then(r => setListingData(r))
+    setIndex(idx + 1)
+  }
+
+  useEffect(() => {
+    setListing()
+  }, [])
+
+  return (
+    <div className="main">
+      <InfiniteScroll
+        dataLength={listingData.length}
+        next={setListing}
+        hasMore={idx < 4}
+        loader={
+          <div className="scroll-loader">
+            <Loader type="ThreeDots" color="#68edd4" height={80} width={80} />
+          </div>
+        }
+        endMessage={
+          <span>
+            <b>You've seen it all</b>
+          </span>
+        }
+      >
+        <div className="flex flex-wrap">
+          {listingData.length > 0 &&
+            listingData
+              .filter(
+                obj =>
+                  obj.name.toLowerCase().indexOf(search.toLowerCase()) !== -1,
+              )
+              .map((item, idx) => (
+                <div key={idx} className="w-1/3 h-12">
+                  <img
+                    className={
+                      item['poster-image'] === 'posterthatismissing.jpg'
+                        ? 'missing-poster'
+                        : '  '
+                    }
+                    key={idx}
+                    src={
+                      item['poster-image'] === 'posterthatismissing.jpg'
+                        ? require(`../../Slices/placeholder_for_missing_posters.png`)
+                        : require(`../../Slices/${item['poster-image']}`)
+                    }
+                    alt={item.name}
+                  />
+                  <p
+                    className={
+                      item.name.length < 13
+                        ? 'listing-item-name'
+                        : 'long-item-name'
+                    }
+                  >
+                    {item.name}
+                  </p>
+                </div>
+              ))}
+        </div>
+      </InfiniteScroll>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    idx: state.listing.idx,
+    listingData: state.listing.listingData,
+    search: state.search.value,
+  }
+}
+
+const mapDispatchToProps = {
+  setListingData,
+  setIndex,
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ListingPage)
